40f2daf412474d6bb21d954e0367fd8d1604519e
Dave Gaeddert <dave.gaeddert@gmail.com>
2017-09-01 18:32:51 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# actor-js-lerna
+[![Docker](https://img.shields.io/badge/dockerhub-actor--js--lerna-22B8EB.svg)](https://hub.docker.com/r/dependencies/actor-js-lerna/)
+[![GitHub release](https://img.shields.io/github/release/dependencies-io/actor-js-lerna.svg)](https://github.com/dependencies-io/actor-js-lerna/releases)
+[![Build Status](https://travis-ci.org/dependencies-io/actor-js-lerna.svg?branch=master)](https://travis-ci.org/dependencies-io/actor-js-lerna)
+[![license](https://img.shields.io/github/license/dependencies-io/actor-js-lerna.svg)](https://github.com/dependencies-io/actor-js-lerna/blob/master/LICENSE)
+
+A [dependencies.io](https://www.dependencies.io)
+[actor](https://www.dependencies.io/docs/actors/) for updating `package.json` dependencies and creating a pull request on GitHub with those changes. Also
+updates `package-lock.json` and `yarn.lock` files if present.
+
+## Usage
+
+### dependencies.yml
+
+```yaml
+collectors:
+- ...
+  actors:
+  - type: js-lerna
+    versions: "L.Y.Y"
+    settings:
+        # please use --concurrency=1 if you provide your own command
+      bootstrap_command: lerna bootstrap --concurrency 1 --hoist
+      # optional contents to put in ~/.npmrc
+      npmrc: |
+        registry=https://skimdb.npmjs.com/registry
+
+      # github options
+      github_labels:  # list of label names
+      - bug
+      github_assignees:  # list of usernames
+      - davegaeddert
+      github_milestone: 3  # milestone number
+      github_base_branch: develop  # branch to make PR against (if something other than your default branch)
+
+      # gitlab options
+      gitlab_assignee_id: 1  # assignee user ID
+      gitlab_labels:  # labels for MR as a list of strings
+      - dependencies
+      - update
+      gitlab_milestone_id: 1  # the ID of a milestone
+      gitlab_target_project_id: 1  # The target project (numeric id)
+      gitlab_remove_source_branch: true  # flag indicating if a merge request should remove the source branch when merging
+      gitlab_target_branch: develop  # branch to make PR against (if something other than your default branch)
+```
+
+### Works well with
+
+- [js-lerna collector](https://www.dependencies.io/docs/collectors/js-lerna/) ([GitHub repo](https://github.com/dependencies-io/collector-js-lerna/))
+
+## Resources
+
+- https://github.com/lerna/lerna
+
+## Support
+
+Any questions or issues with this specific actor should be discussed in [GitHub
+issues](https://github.com/dependencies-io/actor-js-lerna/issues). If there is
+private information which needs to be shared then you can instead use the
+[dependencies.io support](https://app.dependencies.io/support).
---
+++ entrypoint.js
@@ -0,0 +1,93 @@
+const path = require('path')
+const shell = require('shelljs')
+const shellQuote = require('shell-quote')
+const fs = require('fs')
+
+const REPO_PATH = '/repo'
+const TESTING = (process.env.DEPENDENCIES_ENV || 'production') == 'test'
+const ACTOR_ID = process.env.ACTOR_ID
+const GIT_SHA = process.env.GIT_SHA
+const NPMRC = process.env.SETTING_NPMRC
+const dependencies = JSON.parse(process.env.DEPENDENCIES)['dependencies']
+
+shell.set('-e')  // any failing shell commands will fail
+
+function bootstrap() {
+    shell.exec(`cd ${REPO_PATH} && ` + (process.env.SETTING_BOOTSTRAP_COMMAND || 'lerna bootstrap --concurrency 1 --hoist'))
+}
+
+if (NPMRC) {
+    console.log('.npmrc contents found in settings, writing to /home/app/.npmrc...')
+    fs.writeFileSync('/home/app/.npmrc', NPMRC)
+    console.log(NPMRC)
+}
+
+bootstrap()
+
+dependencies.forEach(function(dependency) {
+  console.log(dependency)
+
+  const name = dependency.name
+  const installed = dependency.installed.version
+  const dependencyPath = path.join(REPO_PATH, dependency.path)
+
+  const packageJsonPath = path.join(dependencyPath, 'package.json')
+  const packageJson = require(packageJsonPath)
+  const isDevDependency = packageJson.hasOwnProperty('devDependencies') && packageJson.devDependencies.hasOwnProperty(name)
+
+  const version = dependency.available[0].version
+  let branchName = `${name}-${version}-${ACTOR_ID}`
+  if (dependency.path !== '/') {
+      branchName = branchName + '-' + dependency.path.replace('/', '--')
+  }
+  const msg = `Update ${name} from ${installed} to ${version} in ${dependency.path}`
+
+  let prBody = `${name} has been updated to ${version} in ${dependency.path} by dependencies.io`
+  dependency.available.forEach(function(available) {
+      const content = available.hasOwnProperty('content') ? available.content : '_No content found._'
+      prBody += `\n\n## ${available.version}\n\n${content}`
+  })
+
+  // branch off of the original commit that this build is on
+  shell.exec(`git checkout ${GIT_SHA}`)
+  shell.exec(`git checkout -b ${branchName}`)
+
+  let packageJsonVersionSpecifier
+  if (isDevDependency) {
+    packageJsonVersionSpecifier = packageJson.devDependencies[name]
+  } else {
+    packageJsonVersionSpecifier = packageJson.dependencies[name]
+  }
+
+  let packageJsonVersionRangeSpecifier = ''
+  if (packageJsonVersionSpecifier.startsWith('^')) {
+    packageJsonVersionRangeSpecifier = '^'
+  } else if (packageJsonVersionSpecifier.startsWith('~')) {
+    packageJsonVersionRangeSpecifier = '~'
+  }
+
+  const versionWithRangeSpecifier = packageJsonVersionRangeSpecifier + version
+
+  let npmInstallOpts = '--ignore-scripts --quiet --no-package-lock'
+  if (packageJsonVersionRangeSpecifier === '') {
+      npmInstallOpts += ' --save-exact'
+  }
+
+  // update package.json and then re-run lerna bootstrap
+  shell.exec(`cd ${dependencyPath} && npm install ${npmInstallOpts} ${name}@${versionWithRangeSpecifier}`)
+  bootstrap()
+
+  console.log('This is the git status after performing the update:')
+  shell.exec('git status')
+
+  shell.exec('git add .')
+  shell.exec(`git commit -m "${msg}"`)
+
+  if (!TESTING) {
+    shell.exec(`git push --set-upstream origin ${branchName}`)
+    shell.exec(shellQuote.quote(['pullrequest', '--branch', branchName, '--title', msg, '--body', prBody]))
+  }
+
+  dependencyJSON = JSON.stringify({'dependencies': [dependency]})
+  console.log(`BEGIN_DEPENDENCIES_SCHEMA_OUTPUT>${dependencyJSON}<END_DEPENDENCIES_SCHEMA_OUTPUT`)
+})
